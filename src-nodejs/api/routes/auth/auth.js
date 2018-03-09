6d6c8a1b91363c42e07dcc325f3bf79afe9c6b19
Chad Lewis <lewischa@sonoma.edu>
2018-03-08 19:11:27 -0800

    Integrate JWT with /login route

--- src-nodejs/api/routes/auth/auth.js
+++ src-nodejs/api/routes/auth/auth.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const authDatabase = require('./databaseAccessor');
 const errors = require('../../../errors');
+const { generateToken, verifyToken } = require('./token');
 
 router.post('/login', (req, res, next) => {
+    console.log('req.body', req.body);
     authDatabase.checkHash(req.body)
         .then((authenticated) => {
             console.log('did we get to authenticate?');
             if (authenticated) {
-                res.status(200).json({message: 'auth successful'});
+                return generateToken({ usernameOrEmail: req.body.usernameOrEmail });
             } else {
-                res.status(errors.unauthorizedError.status).json({
-                    message: errors.unauthorizedError.message
-                });
+                throw errors.unauthorizedError;
             }
+        })
+        .then((token) => {
+            res.json({ token });
+        })
+        .catch((error) => {
+            console.log('setting the status again');
+            res.status(error.status).json({
+                error: {
+                    message: error.message
+                }
+            });
         });
 });
 
