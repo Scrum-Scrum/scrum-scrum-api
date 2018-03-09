e9808da313bb022e17f1ae8dc23fe2cf4b22e088
Chad Lewis <lewischa@sonoma.edu>
2018-03-08 19:12:52 -0800

    Add token error to errors/index.js

--- src-nodejs/errors/index.js
+++ src-nodejs/errors/index.js
@@ -1,6 +1,4 @@
-const serverError = new Error(
-    'An error occurred talking to the database'
-);
+const serverError = new Error('An error occurred talking to the database');
 serverError.status = 500;
 
 const forbiddenError = new Error('Forbidden');
@@ -9,12 +7,16 @@ forbiddenError.status = 403;
 const unauthorizedError = new Error('Not authorized');
 unauthorizedError.status = 401;
 
+const invalidTokenError = new Error('Invalid or expired token');
+invalidTokenError.status = 401;
+
 const badRequestError = new Error('Bad request');
 badRequestError.status = 404;
 
 module.exports = {
     badRequestError,
     forbiddenError,
+    invalidTokenError,
     serverError,
     unauthorizedError
 };
